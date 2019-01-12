b3f0f1b385c0b89b184ba438647f393a4728c4a0
ash2osh <orochimaru.ash@gmail.com>
2019-01-12 09:01:13 +0200

    fixed guest checkout infinite redirect loop

--- inc/ajax_payment_sucess.php
+++ inc/ajax_payment_sucess.php
@@ -1,12 +1,13 @@
 <?php
 
-function ash2osh_faw_payment_recieved() {
+function ash2osh_faw_payment_recieved()
+{
     $output = ['status' => 10];
-    $merchantRefNum = (isset($_POST['merchantRefNum']) && $_POST['merchantRefNum']) ? $_POST['merchantRefNum'] : FALSE;
+    $merchantRefNum = (isset($_POST['merchantRefNum']) && $_POST['merchantRefNum']) ? $_POST['merchantRefNum'] : false;
     if ($merchantRefNum) {
-        $order = wc_get_order($merchantRefNum);  
+        $order = wc_get_order($merchantRefNum);
         if ($order->get_user_id() === get_current_user_id()) {
-            $order->update_meta_data('_rec_faw_pay' , 1);
+            $order->update_meta_data('_rec_faw_pay', 1);
             $order->save();//dont forget
             $output = ['status' => 20];
         }
@@ -16,4 +17,4 @@ function ash2osh_faw_payment_recieved() {
 }
 
 add_action('wp_ajax_ash2osh_faw_payment_recieved', 'ash2osh_faw_payment_recieved');
-
+add_action('wp_ajax_nopriv_ash2osh_faw_payment_recieved', 'ash2osh_faw_payment_recieved');//for guest checkout
